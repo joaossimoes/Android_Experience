599ce764822d9c958c57f394f34ad50bd388a3cb
joaossimoes <34171469+joaossimoes@users.noreply.github.com>
2021-07-19 16:17:48 +0100

    Circuit and Player scripts
    Theres now a player entity that travels through circuit checkpoints

---
+++ Tap Racer/Tap Racer/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    List<GameObject> checkpoints;
+    Circuit circuit;
+    Vector3 targetPos;
+    int checkpointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        circuit = FindObjectOfType<Circuit>();
+        checkpoints = circuit.GetCheckpoints();
+        checkpointIndex = 0;
+        transform.position = checkpoints[0].transform.position;
+        targetPos = checkpoints[checkpointIndex + 1].transform.position;
+        checkpointIndex++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, 1f * Time.deltaTime);
+        TargetPosition();
+    }
+
+    void TargetPosition()
+    {
+        if (Vector2.Distance(transform.position, targetPos) < Mathf.Epsilon)
+        {
+            checkpointIndex++;
+            if (checkpointIndex > checkpoints.ToArray().Length - 1) {checkpointIndex = 0;}
+            targetPos = checkpoints[checkpointIndex].transform.position;
+        }
+
+    }
+}
